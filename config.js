c452b39e80c67e17ff4d67d10f41a02800fc4fb5
paddy <954152198@qq.com>
2019-11-28 16:57:47 +0800

    push to remote

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  RINGCENTRAL_CLIENTID: "Wspvji61Ta6a7Sm8W_R9VA",
+  RINGCENTRAL_CLIENTSECRET: "mPjovEICQOSM4dE5wqSagQ2CKT0OLiSn2sBCwGiAFP_Q",
+  RINGCENTRAL_SERVER: "https://platform.devtest.ringcentral.com"
+};
