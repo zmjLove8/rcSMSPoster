0f2101b87ae0d5462f4e96485bbe096f508e23da
zmjLove8 <954152198@qq.com>
2019-11-28 17:04:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  RINGCENTRAL_CLIENTID: "Wspvji61Ta6a7Sm8W_R9VA",
-  RINGCENTRAL_CLIENTSECRET: "mPjovEICQOSM4dE5wqSagQ2CKT0OLiSn2sBCwGiAFP_Q",
-  RINGCENTRAL_SERVER: "https://platform.devtest.ringcentral.com"
+  RINGCENTRAL_CLIENTID: "xxxxxx",
+  RINGCENTRAL_CLIENTSECRET: "xxxxxx",
+  RINGCENTRAL_SERVER: "xxxxxx"
 };
